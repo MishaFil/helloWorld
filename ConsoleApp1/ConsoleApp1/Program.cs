7cbf58f4cc4736579d5d116f08cd86c5ea53b583
DashaUs <dahausanova31@gmail.com>
2023-05-17 20:31:33 +0700

    message

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hello
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Opa");
+            Console.ReadKey();
+        }
+    }
+}
